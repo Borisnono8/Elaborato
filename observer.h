761ab385cd64e7e122357cbf1b14c2d48f83348d
Boris Nono <157750920+Borisnono8@users.noreply.github.com>
2024-11-12 20:59:09 -0800

    Update observer.h

--- observer.h
+++ observer.h
@@ -5,14 +5,11 @@
 #ifndef NONO_LABPROGRAMMAZIONE_OBSERVER_H
 #define NONO_LABPROGRAMMAZIONE_OBSERVER_H
 
-#include <string>
-using namespace std;
-
+// Classe astratta che rappresenta un osservatore
 class Observer {
 public:
-    virtual ~Observer() = default;
-
-    virtual void update(const string& listName) = 0;
+    // Metodo virtuale puro che deve essere implementato dagli osservatori concreti
+    virtual void update(const ShoppingList& list) = 0;
 };
 
 #endif //NONO_LABPROGRAMMAZIONE_OBSERVER_H
