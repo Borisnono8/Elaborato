726a88ddaa12ed552d214bacd2963a4d58375408
Boris Nono <157750920+Borisnono8@users.noreply.github.com>
2025-02-11 01:55:03 +0100

    aggiunto del distruttore virtuale

--- observer.h
+++ observer.h
@@ -1,14 +1,19 @@
 
-//
-
 #ifndef NONO_LABPROGRAMMAZIONE_OBSERVER_H
 #define NONO_LABPROGRAMMAZIONE_OBSERVER_H
 
 // Classe astratta che rappresenta un osservatore
 class Observer {
 public:
+    virtual ~Observer() = default; // Distruttore virtuale
+
     // Metodo virtuale puro che deve essere implementato dagli osservatori concreti
-    virtual void update(const ShoppingList& list) = 0;
+    virtual void update() = 0;
+
+    // Metodo aggiuntivo per aggiornare l'osservatore
+    virtual void aggiorna() {
+        // Implementazione del metodo aggiorna
+    }
 };
 
 #endif //NONO_LABPROGRAMMAZIONE_OBSERVER_H
