9c7976bc63ca01a82a7a94383c1aeda6b10905bd
Boris Nono <157750920+Borisnono8@users.noreply.github.com>
2025-02-11 01:51:22 +0100

    aggiunta di metodi

--- item.h
+++ item.h
@@ -1,12 +1,20 @@
 
-// Classe che rappresenta un oggetto nella lista della spesa
+#pragma once
+#include <stdexcept> // Per std::invalid_argument
+#include <utility>
 
 class Item {
 public:
     std::string name;     // Nome dell'oggetto
     std::string category; // Categoria dell'oggetto
     int quantity;         // Quantità dell'oggetto
+    bool comprato;        // Stato dell'oggetto (comprato o no)
 
     // Costruttore per inizializzare un oggetto Item
-    Item(std::string n, std::string c, int q) : name(n), category(c), quantity(q) {}
+    Item(std::string n, std::string c, const int q) : name(std::move(n)), category(std::move(c)), quantity(q), comprato(false) {
+        if (q < 0) {
+            throw std::invalid_argument("La quantità non può essere negativa");
+        }
+    }
 };
+
